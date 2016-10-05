5a73560222e9e534028d7cab0544e7f728420a88
David Gatti <office@gatti.pl>
2016-10-06 00:37:21 +0200

    Added missing comments

--- integer/index.js
+++ integer/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
+//
+//	Load our C++ app as a module
+//
 let addon = require('./build/Release/integer');
 
+//
+//	Create two variables as buffers
+//
 let nrOne = new Buffer("1");
 let nrTwo = new Buffer("10");
 
+//
+//	Call the C++ function with our numbers, and store the result in a new
+//	variable
+//
 let sum = addon.sum(nrOne, nrTwo);
 
+//
+//	Display the result of the C++ calculation
+//
 console.log("The sum of %s and %s is %s: ", nrOne, nrTwo, sum.readIntLE());
